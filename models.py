d0b2e0046cbda9b7c920455ebc014624363cedb0
galirkil <galirkil@gmail.com>
2024-02-14 15:28:41 +0300

    Delete __str__ overriding, update docstring

--- models.py
+++ models.py
@@ -1,20 +1,12 @@
 class PhoneBookItem:
-    """
-    Класс, представляющий запись в телефонном справочнике.
-    """
-
-    def __init__(self, last_name: str, first_name: str, middle_name: str,
-                 organization: str, work_phone: str, personal_phone: str):
+    """Представляет объект записи телефонного справочника."""
+    def __init__(self, item_id: int, last_name: str, first_name: str,
+                 middle_name: str, organization: str, work_phone: str,
+                 personal_phone: str):
+        self.item_id = item_id
         self.last_name = last_name
         self.first_name = first_name
         self.middle_name = middle_name
         self.organization = organization
         self.work_phone = work_phone
         self.personal_phone = personal_phone
-
-    def __str__(self) -> str:
-        """
-        Возвращает строковое представление записи.
-        """
-        return f"{self.last_name}, {self.first_name}, {self.middle_name}, " \
-               f"{self.organization}, {self.work_phone}, {self.personal_phone}"
